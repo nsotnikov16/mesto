9c49ad2f1e39147792e62fd6cb1c1a2f266003fe
Nikita Sotnikov <nsotnikov16@yandex.ru>
2021-04-21 04:07:26 +0500

    feat: добавить js

--- script/index.js
+++ script/index.js
@@ -1,10 +1,14 @@
 const editButton = document.querySelector('.edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close');
+const profileUserName = document.querySelector('.profile__username');
+const profileInfo = document.querySelector('.profile__info');
+
+
 
 function togglePopup(event) {
     event.preventDefault();
-    popup.classList.toggle('popup_opened')
+    popup.classList.toggle('popup_opened');
 }
 
 function handleOverlayClick(event) {
