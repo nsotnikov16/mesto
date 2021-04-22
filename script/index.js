359e306f4de6b412fd8a0f75d141269d34568eb0
Nikita Sotnikov <nsotnikov16@yandex.ru>
2021-04-22 23:26:43 +0500

    fix: исправить замечания

--- script/index.js
+++ script/index.js
@@ -1,40 +1,43 @@
-const editButton = document.querySelector('.edit-button');
+const editButton = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close');
 const profileUserName = document.querySelector('.profile__username');
 const profileInfo = document.querySelector('.profile__info');
-let popupUserName = document.querySelector('.popup__username');
-let popupInfo = document.querySelector('.popup__info');
+let popupUserName = document.querySelector('.popup__input_username');
+let popupInfo = document.querySelector('.popup__input_info');
 let saveButton = document.querySelector('.popup__save');
 let form = document.querySelector('.popup__form');
-
-function togglePopup(event) {
-    event.preventDefault();
-    popup.classList.toggle('popup_opened');
-    textPopupProfile();
-}
-
-function handleOverlayClick(event) {
-    if (event.target === event.currentTarget) {
-        togglePopup(event);
-    }
-}
 
 function textPopupProfile () {
     popupUserName.value = profileUserName.textContent;
     popupInfo.value = profileInfo.textContent;
 }
 
+function openPopup() {
+    popup.classList.add('popup_opened');
+    textPopupProfile();
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+function handleOverlayClick(event) {
+    if (event.target === event.currentTarget) {
+        openPopup() || closePopup();
+    } 
+}
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileUserName.textContent = popupUserName.value;
     profileInfo.textContent = popupInfo.value;
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
 
 form.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
+editButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 popup.addEventListener('click', handleOverlayClick);
 
 
