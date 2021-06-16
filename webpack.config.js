4fb170f2f56484a7988c44be172acb929b199c1d
Nikita Sotnikov <nsotnikov16@yandex.ru>
2021-06-16 13:32:30 +0300

    hotfix: bug comments 2

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         filename: 'main.js',
         publicPath: ''
     },
-    devtool: 'source-map',
+    //devtool: 'source-map',
     mode: 'development',
     devServer: {
         contentBase: path.resolve(__dirname, './dist'),
