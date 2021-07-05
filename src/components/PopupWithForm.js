2cfe8dfd1ea9b009dddfda2c152cc7fd4ed5f68c
Nikita Sotnikov <nsotnikov16@yandex.ru>
2021-07-05 19:18:41 +0500

    hotfix: bug-comments and formatting the code

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,45 +1,49 @@
-import Popup from './Popup.js'
+import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
-    constructor ( {selectorPopup}, textButtonDefault, textButtonLoading, submitForm) {
-        super({selectorPopup});
-        this._submitForm = submitForm;
-        this._inputList = Array.from(this.form.querySelectorAll('.popup__input'));
-        this._buttonSave = this._popupElement.querySelector('.popup__save')
-        this.other = null;
-        this.textButtonDefault = textButtonDefault;
-        this._textButtonLoading = textButtonLoading;
-    }
+  constructor(
+    { selectorPopup },
+    textButtonDefault,
+    textButtonLoading,
+    submitForm
+  ) {
+    super({ selectorPopup });
+    this._submitForm = submitForm;
+    this._inputList = Array.from(this.form.querySelectorAll(".popup__input"));
+    this._buttonSave = this._popupElement.querySelector(".popup__save");
+    this.other = null;
+    this.textButtonDefault = textButtonDefault;
+    this._textButtonLoading = textButtonLoading;
+  }
 
-    _getInputValues () {
-        this._formValues = {};
-        this._inputList.forEach(input => {
-            this._formValues[input.name] = input.value;
-        })
+  _getInputValues() {
+    this._formValues = {};
+    this._inputList.forEach((input) => {
+      this._formValues[input.name] = input.value;
+    });
 
-        return this._formValues;
-    }
-    
-    editTextButton(text) {
-        this._buttonSave.textContent = text
-    }
+    return this._formValues;
+  }
 
-    _handleSubmitForm (evt) {
-        evt.preventDefault();
-        this.editTextButton(this._textButtonLoading)
-        this._submitForm(this._getInputValues());
-        this.close()
-    }
+  editTextButton(text) {
+    this._buttonSave.textContent = text;
+  }
 
-    setEventListeners () {
-        super.setEventListeners();
-        this.form = this._popupElement.querySelector('.popup__form');
-        this.form.addEventListener('submit', (evt) => this._handleSubmitForm(evt))
-    }
+  _handleSubmitForm(evt) {
+    evt.preventDefault();
+    this.editTextButton(this._textButtonLoading);
+    this._submitForm(this._getInputValues());
+  }
 
-    close () {
-        super.close();
-        this.form.reset();
-    }
+  setEventListeners() {
+    super.setEventListeners();
+    this.form = this._popupElement.querySelector(".popup__form");
+    this.form.addEventListener("submit", (evt) => this._handleSubmitForm(evt));
+  }
 
+  close() {
+    super.close();
+    this.form.reset();
+    this.editTextButton(this.textButtonDefault);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,91 +1,179 @@
-import './pages/index.css';
+import "./pages/index.css";
 import Card from "./components/Card.js";
 import PopupWithImage from "./components/PopupWithImage.js";
-import Section from './components/Section.js';
-import UserInfo from './components/UserInfo.js';
-import Popup from './components/Popup.js';
+import Section from "./components/Section.js";
+import UserInfo from "./components/UserInfo.js";
+import Popup from "./components/Popup.js";
 import PopupWithForm from "./components/PopupWithForm.js";
-import FormValidator from './components/FormValidator.js';
-import Api from './components/Api.js';
-import { hideInputErrorForm, hoverEditAvatar } from './utils/utils.js';
-import { addButton, editButton, profileInputName, profileInputInfo, idTemplate, selectors, config, avatarChange, editAvatar, apiRequest, avatar} from './utils/constants.js';
+import FormValidator from "./components/FormValidator.js";
+import Api from "./components/Api.js";
+import {
+  hideInputErrorForm,
+  hoverEditAvatar,
+} from "./utils/utils.js";
+import {
+  addButton,
+  editButton,
+  profileInputName,
+  profileInputInfo,
+  idTemplate,
+  selectors,
+  config,
+  avatarChange,
+  editAvatar,
+  apiRequest,
+  avatar,
+} from "./utils/constants.js";
 
 let userInfoServer = null;
 
 function createCard(item) {
-    const card = new Card (item, idTemplate, userInfoServer, () => api.putLikeCard(card.id), () => api.deleteLikeCard(card.id),  () => popupWithImage.open(item), (id, element) => {
-        popupConfirm.other = {id, element}; 
-        popupConfirm.open();
-    })
-    const cardElement = card.generateCard();
-    return cardElement
+  const card = new Card(
+    item,
+    idTemplate,
+    userInfoServer,
+    () => api.putLikeCard(card.id),
+    () => api.deleteLikeCard(card.id),
+    () => popupWithImage.open(item),
+    (id, element) => {
+      popupConfirm.other = { id, element };
+      popupConfirm.open();
+    }
+  );
+  const cardElement = card.generateCard();
+  return cardElement;
 }
 
-const cardsPage = new Section({
-    renderer: (item) => cardsPage.addItemAppend(createCard(item))
-}, selectors.elements)
+function handleRejected(err, popup) {
+    popupError.showErrorServer(err);
+    popup.editTextButton(popup.textButtonDefault);
+} // Не знаю как лучше будет именно для проектной работы, но считаю что в реальной ситуации, лучше всё таки при ошибке с сервера не закрывать и не сбрасывать форму
+
+const cardsPage = new Section(
+  {
+    renderer: (item) => cardsPage.addItemAppend(createCard(item)),
+  },
+  selectors.elements
+);
 
 const api = new Api(apiRequest);
 
-const popupNewCard = new PopupWithForm({selectorPopup: selectors.popupNewPlaceSelector}, 'Создать', 'Создание...', inputsValue => {
-    api.addNewCardServer(inputsValue).then(result => {cardsPage.addItemPrepend(createCard(result)); popupNewCard.editTextButton(popupNewCard.textButtonDefault)}).catch(err => popupError.showErrorServer(err));
-})
+const popupNewCard = new PopupWithForm(
+  { selectorPopup: selectors.popupNewPlaceSelector },
+  "Создать",
+  "Создание...",
+  (inputsValue) => {
+    api
+      .addNewCardServer(inputsValue)
+      .then((result) => {
+        cardsPage.addItemPrepend(createCard(result));
+        popupNewCard.close();
+      })
+      .catch((err) => handleRejected(err, popupNewCard));
+  }
+);
 
-const popupProfile = new PopupWithForm ({selectorPopup: selectors.popupProfileSelector}, 'Сохранить', 'Сохранение...', inputsValue => {
-    api.editProfile(inputsValue).then(result => {profile.setUserInfo(result); popupProfile.editTextButton(popupProfile.textButtonDefault)}).catch(err => popupError.showErrorServer(err));
+const popupProfile = new PopupWithForm(
+  { selectorPopup: selectors.popupProfileSelector },
+  "Сохранить",
+  "Сохранение...",
+  (inputsValue) => {
+    api
+      .editProfile(inputsValue)
+      .then((result) => {
+        profile.setUserInfo(result);
+        popupProfile.close();
+      })
+      .catch((err) => handleRejected(err, popupProfile));
+  }
+);
+
+const popupWithImage = new PopupWithImage({
+  selectorPopup: selectors.popupWithImageSelector,
 });
 
-const popupWithImage = new PopupWithImage ({selectorPopup: selectors.popupWithImageSelector});
-
-const profile = new UserInfo ({userName: selectors.profileUserName, userInfo: selectors.profileInfo, userAvatar: selectors.profileAvatar});
-const {name: username, info: userinfo, avatar: useravatar} = profile.getUserInfo();
-
-const popupUpdate = new PopupWithForm({selectorPopup: selectors.popupUpdate}, 'Сохранить', 'Сохранение...',inputsValue => {
-    api.editAvatar(inputsValue).then(result => {avatar.src = result.avatar; popupUpdate.editTextButton(popupUpdate.textButtonDefault)}).catch(err => popupError.showErrorServer(err));
-})
-
-const popupConfirm = new PopupWithForm ({selectorPopup: selectors.popupConfirm}, 'Да', 'Удаление...', () => {
-    api.deleteCard(popupConfirm.other.id).then(() => {popupConfirm.other.element.remove(); popupConfirm.editTextButton(popupConfirm.textButtonDefault)}).catch(err => popupError.showErrorServer(err));
+const profile = new UserInfo({
+  userName: selectors.profileUserName,
+  userInfo: selectors.profileInfo,
+  userAvatar: selectors.profileAvatar,
 });
 
-const popupError = new Popup ({selectorPopup: selectors.popupError});
+const {
+  name: username,
+  info: userinfo,
+  avatar: useravatar,
+} = profile.getUserInfo();
 
-const validatorNewPlace = new FormValidator (config, popupNewCard.form);
+const popupUpdate = new PopupWithForm(
+  { selectorPopup: selectors.popupUpdate },
+  "Сохранить",
+  "Сохранение...",
+  (inputsValue) => {
+    api
+      .editAvatar(inputsValue)
+      .then((result) => {
+        avatar.src = result.avatar;
+        popupUpdate.close();
+      })
+      .catch((err) => handleRejected(err, popupUpdate));
+  }
+);
+
+const popupConfirm = new PopupWithForm(
+  { selectorPopup: selectors.popupConfirm },
+  "Да",
+  "Удаление...",
+  () => {
+    api
+      .deleteCard(popupConfirm.other.id)
+      .then(() => {
+        popupConfirm.other.element.remove();
+        popupConfirm.close();
+      })
+      .catch((err) => handleRejected(err, popupConfirm));
+  }
+);
+
+const popupError = new Popup({ selectorPopup: selectors.popupError });
+
+const validatorNewPlace = new FormValidator(config, popupNewCard.form);
 validatorNewPlace.enableValidation();
 
-const validatorProfile = new FormValidator (config, popupProfile.form);
+const validatorProfile = new FormValidator(config, popupProfile.form);
 validatorProfile.enableValidation();
 
-const validatorUpdate = new FormValidator(config, popupUpdate.form)
+const validatorUpdate = new FormValidator(config, popupUpdate.form);
 validatorUpdate.enableValidation();
 
-addButton.addEventListener('click', () => {
-    hideInputErrorForm(popupNewCard, validatorNewPlace)
-    validatorNewPlace.toggleButtonState();
-    popupNewCard.open();
-})
+addButton.addEventListener("click", () => {
+  hideInputErrorForm(popupNewCard, validatorNewPlace);
+  validatorNewPlace.toggleButtonState();
+  popupNewCard.open();
+});
 
-editButton.addEventListener('click', () => {
-    profileInputName.value = username.textContent;
-    profileInputInfo.value = userinfo.textContent;
+editButton.addEventListener("click", () => {
+  profileInputName.value = username.textContent;
+  profileInputInfo.value = userinfo.textContent;
 
-    hideInputErrorForm(popupProfile, validatorProfile);
-    popupProfile.open();
-})
+  hideInputErrorForm(popupProfile, validatorProfile);
+  popupProfile.open();
+});
 
-avatarChange.addEventListener('mouseover', hoverEditAvatar);
-avatarChange.addEventListener('mouseout', hoverEditAvatar);
+avatarChange.addEventListener("mouseover", hoverEditAvatar);
+avatarChange.addEventListener("mouseout", hoverEditAvatar);
 
-editAvatar.addEventListener('click', () => {
-    hideInputErrorForm(popupUpdate, validatorUpdate);
-    validatorUpdate.toggleButtonState();
-    popupUpdate.open()
-})
+editAvatar.addEventListener("click", () => {
+  hideInputErrorForm(popupUpdate, validatorUpdate);
+  validatorUpdate.toggleButtonState();
+  popupUpdate.open();
+});
 
-Promise.all([api.getUserData(), api.getInitialCards()]).then(([userdata, cards]) => {
+Promise.all([api.getUserData(), api.getInitialCards()])
+  .then(([userdata, cards]) => {
     username.textContent = userdata.name;
     userinfo.textContent = userdata.about;
     useravatar.src = userdata.avatar;
     userInfoServer = userdata;
     cardsPage.renderItemsServer(cards);
-}).catch(err => popupError.showErrorServer(err))
+  })
+  .catch((err) => popupError.showErrorServer(err));
